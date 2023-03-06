0b8afec72362a749eecdef79abeb8501a99d5991
886152 <886152@886152W1>
2023-03-05 23:29:13 -0700

    New Project Creation made by songlie xu create a new semester info web page.

---
+++ src/java/dataaccess/ConnectionPool.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+import java.sql.*;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author 886152
+ */
+public class ConnectionPool {
+
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+
+            dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/app0");
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+    }
+
+    ;
+
+    public static synchronized ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void freeConnection(Connection c) {
+
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
---
+++ web/WEB-INF/users.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : users
+    Created on : 5-Mar-2023, 9:58:14 PM
+    Author     : 886152
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Manage Users</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
