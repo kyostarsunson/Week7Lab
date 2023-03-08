dc84499ed770f1143978bae3ce488b7026f9368d
886152 <886152@886152W1>
2023-03-08 12:02:51 -0700

    New Project Creation made by songlie xu create a new semester info web page.

--- src/java/Servlets/UserServlet.java
+++ src/java/Servlets/UserServlet.java
@@ -5,12 +5,15 @@
  */
 package Servlets;
 
+import dataaccess.UserDB;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
 
 /**
  *
@@ -21,6 +24,29 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //System.err.println("I am hereffffffffffffffffffffffffffffffffffffff");
+
+        HttpSession mySession = request.getSession();
+
+        UserDB myUserDb = new UserDB();
+        //String emailString = (String) mySession.getAttribute("email");
+        List<User> users = myUserDb.getAll();
+
+        request.setAttribute("users", users);
+
+        ///
+        String email = request.getParameter("email");
+        if (email != null) {
+
+            email = email.replaceAll(" ", "+");
+            email = email.replaceAll("'", "");
+            System.out.println("Servlets.UserServlet.getParameter(),email=" + email);
+            User user = myUserDb.getByEmail(email);
+
+            System.out.println("Servlets.UserServlet.doGet(),user=" + user);
+            request.setAttribute("selectuser", user);
+        }
+
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
     }
 
@@ -36,6 +62,28 @@ public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+//        HttpSession mySession = request.getSession();
+//
+//        String email = (String) mySession.getAttribute("email");
+//
+//        String actionString = request.getParameter("action");
+        //UserDB myUserDb = new UserDB();
+        //String userString = myUserDb.getAll().toString();
+        //request.setAttribute("users", myUserDb.getAll());
+        //doGet(request, response);
+        //getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+        //System.err.println("Users info: =" + userString);
+//        try {
+//
+//            switch (actionString) {
+//                case "add": {
+//
+//                }
+//                ;
+//            };
+//
+//        } catch (Exception e) {
+//        }
     }
 
 }
